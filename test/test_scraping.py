e748361597040e919febeb0c33222d2f50b7294e
exopyl <exopyl74@mail.ru>
2022-01-25 23:20:52 +0100

    test for scrap_yahoo_list.get_list_NASDAQ

--- test/test_scraping.py
+++ test/test_scraping.py
@@ -1,6 +1,17 @@
 import pytest
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from scraping import scrap_yahoo_list
 
 class TestScraping:
 
-    def test_1(self):
-        assert(True)
+    def test_get_list_nasdaq(self):
+        # action
+        df = scrap_yahoo_list.get_list_NASDAQ()
+
+        # expectations
+        df.to_csv("./test/generated/list_nasdaq.csv")
+        df_generated = pd.read_csv("./test/generated/list_nasdaq.csv")
+        df_ref = pd.read_csv("./test/references/list_nasdaq.csv")
+        assert_frame_equal(df_generated, df_ref)
