57fc0ffbe4001687a86ae86ed993a1411e682fb4
exopyl <exopyl74@mail.ru>
2022-01-30 19:38:17 +0100

    test for scrap_yahoo_list.get_list_yahoo_SP500 & get_list_DOW

--- test/test_scraping.py
+++ test/test_scraping.py
@@ -15,3 +15,26 @@ class TestScraping:
         df_generated = pd.read_csv("./test/generated/list_nasdaq.csv")
         df_ref = pd.read_csv("./test/references/list_nasdaq.csv")
         assert_frame_equal(df_generated, df_ref)
+
+    def test_get_list_yahoo_SP500(self):
+        # action
+        df = scrap_yahoo_list.get_list_yahoo_SP500()
+
+        # expectations
+        df.to_csv("./test/generated/list_sp500.csv")
+        df_generated = pd.read_csv("./test/generated/list_sp500.csv")
+        df_ref = pd.read_csv("./test/references/list_sp500.csv")
+        assert_frame_equal(df_generated, df_ref)
+
+    def test_get_list_dow(self):
+        # action
+        df = scrap_yahoo_list.get_list_DOW()
+
+        # expectations
+        df.to_csv("./test/generated/list_dow.csv")
+        df_generated = pd.read_csv("./test/generated/list_dow.csv")
+        df_ref = pd.read_csv("./test/references/list_dow.csv")
+        assert_frame_equal(df_generated, df_ref)
+       
+
+
